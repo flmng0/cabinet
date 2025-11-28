9eae28016ae3995571a757d6079fd9787b5e1675
Timothy Davis <tmthydvs@gmail.com>
2025-11-28 13:54:43 +0930

    Hide button

--- lib/cabinet_web/live/admin_live/invoice/index.ex
+++ lib/cabinet_web/live/admin_live/invoice/index.ex
@@ -16,9 +16,9 @@ defmodule CabinetWeb.AdminLive.Invoice.Index do
         <.icon name="hero-queue-list" /> Invoices
         <:actions>
           <!-- TODO: Implement the add-new event on this view -->
-          <.button class="btn btn-outline btn-secondary" phx-click="add-invoice">
-            <.icon name="hero-plus" /> Add New
-          </.button>
+          <%!-- <.button class="btn btn-outline btn-secondary" phx-click="add-invoice"> --%>
+          <%!--   <.icon name="hero-plus" /> Add New --%>
+          <%!-- </.button> --%>
         </:actions>
       </.header>
 
